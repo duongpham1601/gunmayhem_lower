4e4a678a0ce447dd5d1544aaa97805a7af7d636b
duongpham1601 <phamquyduong161@gmail.com>
2026-08-20 13:26:41 +0700

    test

---
+++ src/entity.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <SDL3/SDL.h>
+#define MAX_ENTITIES 100
+
+typedef struct {
+    void (*quit)(void);
+    void (*handle_events)(SDL_Event*);
+    void (*update)(float);
+    void (*render)(SDL_Renderer*);   
+} Entity;
+
+extern Entity entities[MAX_ENTITIES];
+extern int entities_count;
